78e6d96d0eef346f9b4ea49672e27e389adfd286
Nicola Toscan <nicolatoscan99@outlook.it>
2024-07-22 12:57:47 +0200

    removed base url

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/query-maker/',
+  // base: '/query-maker/',
   plugins: [
     vue({
       template: { transformAssetUrls }
